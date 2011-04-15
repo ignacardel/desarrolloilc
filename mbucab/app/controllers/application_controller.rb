d38a8b557d0b991b758717ecb98c19c7e31c7790
cotufa9 <cotufa9@8bfa4be5-6424-f957-b6ae-c545fea77958>
2011-04-15 21:53:37 +0000

    prueba

--- mbucab/app/controllers/application_controller.rb
+++ mbucab/app/controllers/application_controller.rb
@@ -5,6 +5,29 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+     def gotogoogle
+
+    redirect_to "https://www.google.com/accounts/o8/ud
+?openid.ns=http://specs.openid.net/auth/2.0
+&openid.ns.pape=http://specs.openid.net/extensions/pape/1.0
+&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select
+&openid.identity=http://specs.openid.net/auth/2.0/identifier_select
+&openid.return_to=http://localhost:3000/clients/new
+&openid.realm=http://localhost:3000/
+&openid.assoc_handle=ABSmpf6DNMw
+&openid.mode=checkid_setup
+&openid.ui.ns=http://specs.openid.net/extensions/ui/1.0
+&openid.ui.mode=popup
+&openid.ui.icon=true
+&openid.ns.ax=http://openid.net/srv/ax/1.0
+&openid.ax.mode=fetch_request
+&openid.ax.type.email=http://axschema.org/contact/email
+&openid.ax.type.language=http://axschema.org/pref/language
+&openid.ax.type.firstname=http://axschema.org/namePerson/first
+&openid.ax.type.lastname=http://axschema.org/namePerson/last
+&openid.ax.required=email,language,firstname,lastname"
+
+   end
   # hola esto es una prueba
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
