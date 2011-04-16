b32ac92763e84e09d7f85dac00e08e9c91f628ee
venezuelanpride23 <venezuelanpride23@8bfa4be5-6424-f957-b6ae-c545fea77958>
2011-04-16 18:42:39 +0000

    lo mismo de la anterior pero subir nuevos orders

---
+++ mbucab/app/models/order.rb
@@ -0,0 +1,15 @@
+class Order < ActiveRecord::Base
+  #Validations
+  validates_presence_of :date
+  validates_presence_of :recipient
+  validates_presence_of :fulladdress
+
+
+
+
+  #Relations
+  has_many :packages
+  belongs_to :client
+  belongs_to :address
+  belongs_to :creditcard
+end
