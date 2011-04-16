cca8edd5a56876bc3f28ea3823c75650d82d6bde
cotufa9 <cotufa9@8bfa4be5-6424-f957-b6ae-c545fea77958>
2011-04-16 03:34:59 +0000

    link en login y register y metodos para q funcionen

--- mbucab/app/controllers/session_controller.rb
+++ mbucab/app/controllers/session_controller.rb
@@ -1,12 +1,32 @@
 class SessionController < ApplicationController
 
-  def user_login
-   #client.authenticate(email)
-   puts "pasojdpaod"
+  def client_login
+
+         request.query_string.split(/&/).inject({}) do |hash, setting|
+           key, val = setting.split(/=/)
+           if key == 'openid.ext1.value.email'
+            usuario, correo  = val.split(/%40/)
+            @email = usuario + '@' + correo
+           end
+         end
+
+    client= Client.find(:first, :conditions => [" account = ?", @email ])
+
+    if client
+     session[:user] = client.account
+     flash[:notice] = client.firstname + " " + client.lastname + " (" + session[:user] + ") has logged in!"
+     redirect_to :controller => 'home', :action => 'index'
+    else
+     flash[:notice] = "\"" + @email + "\" is not in registered!"
+     redirect_to :controller => 'home', :action => 'index'
+    end
+
   end
 
-  def user_logout
-
+  def client_logout
+    session[:user] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to :controller => 'home', :action => 'index'
 
   end
 
