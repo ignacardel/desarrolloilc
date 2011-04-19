6dd2b15d780e0f3034218d27f3c74050e0c72725
venezuelanpride23 <venezuelanpride23@8bfa4be5-6424-f957-b6ae-c545fea77958>
2011-04-19 03:11:57 +0000

    -Se arreglo vista de cliente para que cuando de error se resalten los labels q no se llaman igual al atributo en el modelo -Se arreglo modelo de  direccion y el de cliente para definir como se muestran los atributos en el mensaje de error de la vista -Validacion de fecha de vencimiento de tarjeta de credito en el modelo correspondiente -Se modifico vista de tarjeta de credito pa q la fecha de vencimiento muestre solo mes y ano, hay q decidir si se deja la validacion en el modelo o basta con cambiar la interfaz para q solo muestre fechas futuras

--- trunk/mbucab/app/models/creditcard.rb
+++ trunk/mbucab/app/models/creditcard.rb
@@ -11,12 +11,18 @@ class Creditcard < ActiveRecord::Base
   validates_inclusion_of :number, :in => 1000000000..9999999999, :message=> 'Must have 10 digits'
   
   validates_numericality_of :code
-  validates_inclusion_of :number, :in => 1000..9999, :message=> 'Must have 4 digits'
-
+  validates_inclusion_of :code, :in => 1000..9999, :message=> 'Must have 4 digits'
  
 
+  
   #Relations
   
   belongs_to :client
   has_many :orders
+
+  #Valida que la fecha de expiracion de la tarjeta
+  #de credito sea mayor a la actual
+  def validate
+    errors.add(:expdate, "is invalid") if expdate < Date.today
+  end
 end
