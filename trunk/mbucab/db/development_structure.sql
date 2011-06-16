927a7b1b0a6b91b7b61e55a2b9e5f2f31e58256d
cotufa9 <cotufa9@8bfa4be5-6424-f957-b6ae-c545fea77958>
2011-06-16 20:30:55 +0000

    cambio en la base test

--- trunk/mbucab/db/development_structure.sql
+++ trunk/mbucab/db/development_structure.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "addresses" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "street" varchar(255), "name" varchar(255), "number" varchar(255), "zone" varchar(255), "city" varchar(255), "country" varchar(255), "zip" integer, "latitude" float, "longitude" float, "nickname" varchar(255), "client_id" integer, "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "clients" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "account" varchar(255), "firstname" varchar(255), "middlename" varchar(255), "lastname" varchar(255), "surname" varchar(255), "birthday" date, "phone" integer, "created_at" datetime, "updated_at" datetime, "active" integer);
-CREATE TABLE "companies" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "rif" varchar(255), "phone" varchar(255), "fax" varchar(255), "address" varchar(255), "created_at" datetime, "updated_at" datetime);
+CREATE TABLE "companies" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "rif" varchar(255), "phone" varchar(255), "fax" varchar(255), "address" varchar(255), "created_at" datetime, "updated_at" datetime, "ip_address" varchar(255));
 CREATE TABLE "creditcards" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "number" integer, "expdate" date, "code" integer, "name" varchar(255), "client_id" integer, "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "employees" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "account" varchar(255), "password" varchar(255), "name" varchar(255), "lastname" varchar(255), "role" integer, "created_at" datetime, "updated_at" datetime);
-CREATE TABLE "orders" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "date" datetime, "recipient" varchar(255), "longitude" float, "latitude" float, "status" integer, "collectiondate" datetime, "deliverydate" datetime, "address_id" integer, "client_id" integer, "creditcard_id" integer, "created_at" datetime, "updated_at" datetime, "route_id" integer, "street" varchar(255), "name" varchar(255), "number" integer, "zone" varchar(255), "city" varchar(255), "country" varchar(255), "zip" integer, "order_type" integer, "extra" float, "company_id" integer, "external" integer);
+CREATE TABLE "orders" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "date" datetime, "recipient" varchar(255), "longitude" float, "latitude" float, "status" integer, "collectiondate" datetime, "deliverydate" datetime, "address_id" integer, "client_id" integer, "creditcard_id" integer, "created_at" datetime, "updated_at" datetime, "route_id" integer, "street" varchar(255), "name" varchar(255), "number" varchar(255), "zone" varchar(255), "city" varchar(255), "country" varchar(255), "zip" integer, "order_type" integer, "extra" float, "company_id" integer, "external" integer);
 CREATE TABLE "packages" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "description" varchar(255), "weight" float, "price" float, "order_id" integer, "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "routes" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "employee_id" integer, "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
@@ -39,3 +39,7 @@ INSERT INTO schema_migrations (version) VALUES ('20110531232536');
 INSERT INTO schema_migrations (version) VALUES ('20110607235821');
 
 INSERT INTO schema_migrations (version) VALUES ('20110609194500');
+
+INSERT INTO schema_migrations (version) VALUES ('20110611225431');
+
+INSERT INTO schema_migrations (version) VALUES ('20110615204117');
