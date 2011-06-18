91382e53b472afa28445ef95bd938d894acf4fe9
leonardof25 <leonardof25@8bfa4be5-6424-f957-b6ae-c545fea77958>
2011-06-18 02:01:06 +0000

    Pruebas unitarias y funcionales! Ademas de arreglar un peo en el modelo de address!

--- mbucab/test/unit/address_test.rb
+++ mbucab/test/unit/address_test.rb
@@ -1,8 +1,38 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
+
+#require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+
+  fixtures :addresses
+
+  def test_address
+
+    address = Address.new :street => addresses(:home).street,
+                          :name => addresses(:home).name,
+                          :number => addresses(:home).number,
+                          :zone => addresses(:home).zone,
+                          :city => addresses(:home).city,
+                          :country => addresses(:home).country,
+                          :zip => addresses(:home).zip,
+                          :latitude => addresses(:home).latitude,
+                          :longitude => addresses(:home).longitude,
+                          :nickname => "MyString",
+                          :client_id => addresses(:home).client_id
+    assert !address.save
+
+    address.nickname = "Mi casita"
+
+    assert address.save
+
+    address_copy = Address.find(address.id)
+
+    assert_equal address.zip, address_copy.zip
+
+    address.nickname = "Mi Casita Nueva"
+
+    assert address.save
+    assert address.destroy
   end
 end
